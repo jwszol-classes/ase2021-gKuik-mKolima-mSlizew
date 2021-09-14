ac2c41f3e91ff19de7a4809a02d748400ee101ca
GabrielQuik <cegla1002@tlen.pl>
2021-09-14 20:26:55 +0200

    merge dataframes

--- app.py
+++ app.py
@@ -2,6 +2,34 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.master("local").appName("MyApp").getOrCreate()
 # 'lpep_pickup_datetime', 'lpep_dropoff_datetime'
-df = spark.read.option("delimiter", ',').csv('./*.csv', header=True,).select('','trip_distance')
-print(df.count())
-df.show(5)
+df_green = (
+    spark.read.option("delimiter", ",")
+    .csv(
+        "./dataset/green*.csv",
+        header=True,
+    )
+    .select("lpep_pickup_datetime", "lpep_dropoff_datetime", "trip_distance")
+)
+
+df_yellow = (
+    spark.read.option("delimiter", ",")
+    .csv(
+        "./dataset/yellow*.csv",
+        header=True,
+    )
+    .select("tpep_pickup_datetime", "tpep_dropoff_datetime", "trip_distance")
+)
+
+df_green = df_green.withColumnRenamed("lpep_pickup_datetime", "pickup_time").withColumnRenamed(
+    "lpep_dropoff_datetime", "drop_time"
+)
+df_yellow = df_yellow.withColumnRenamed("tpep_pickup_datetime", "pickup_time").withColumnRenamed(
+    "tpep_dropoff_datetime", "drop_time"
+)
+# print(df_green.count(), df_yellow.count())
+
+
+df = df_green.union(df_yellow)
+df.show(10)
+
+# print(df.count())
