5b7bdbabb9cb8da5f76122fdf4ca83cabfdba8fd
GabrielQuik <cegla1002@tlen.pl>
2021-09-14 22:08:48 +0200

    add calculations

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, functions as F, types
 
 spark = SparkSession.builder.master("local").appName("MyApp").getOrCreate()
-# 'lpep_pickup_datetime', 'lpep_dropoff_datetime'
+
 df_green = (
     spark.read.option("delimiter", ",")
     .csv(
@@ -9,6 +9,9 @@ df_green = (
         header=True,
     )
     .select("lpep_pickup_datetime", "lpep_dropoff_datetime", "trip_distance")
+)
+df_green = df_green.withColumnRenamed("lpep_pickup_datetime", "pickup_time").withColumnRenamed(
+    "lpep_dropoff_datetime", "drop_time"
 )
 
 df_yellow = (
@@ -19,17 +22,21 @@ df_yellow = (
     )
     .select("tpep_pickup_datetime", "tpep_dropoff_datetime", "trip_distance")
 )
-
-df_green = df_green.withColumnRenamed("lpep_pickup_datetime", "pickup_time").withColumnRenamed(
-    "lpep_dropoff_datetime", "drop_time"
-)
 df_yellow = df_yellow.withColumnRenamed("tpep_pickup_datetime", "pickup_time").withColumnRenamed(
     "tpep_dropoff_datetime", "drop_time"
 )
-# print(df_green.count(), df_yellow.count())
-
 
 df = df_green.union(df_yellow)
-df.show(10)
+
+
+time_diff = (F.unix_timestamp("drop_time") - (F.unix_timestamp("pickup_time")).cast(types.DoubleType())) / 3600
+
+df = df.withColumn("Duration", time_diff)
+
+velocity = (F.col("trip_distance").cast(types.DoubleType()) / F.col("Duration"))
+df = df.withColumn("Velocity", velocity)
+
+print(df.groupBy().avg("Velocity").collect())
+# df.write()
 
 # print(df.count())
